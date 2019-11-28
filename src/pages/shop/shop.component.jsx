2c2d99798b7b61a6d1759d2d79a5ff9c98ebcfa9
SimasDei <simondeiman@gmail.com>
2019-11-28 19:37:43 +0200

    refactor collections fetch to graph-ql

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPage from '../collection/collection.component';
 
 const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
     <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
